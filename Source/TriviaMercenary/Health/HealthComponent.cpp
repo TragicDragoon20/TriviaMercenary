a6cd96c4ff65d85a07fc1d128772024589c5fb58
Jamie Carnell <jc228467@falmouth.ac.uk>
2022-08-19 10:52:08 +0100

    Created Health Bar which is updated whenever the player's health updates

--- Source/TriviaMercenary/Health/HealthComponent.cpp
+++ Source/TriviaMercenary/Health/HealthComponent.cpp
@@ -9,8 +9,6 @@ UHealthComponent::UHealthComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = false;
-
-	health = startingHealth;
 }
 
 
@@ -18,6 +16,8 @@ UHealthComponent::UHealthComponent()
 void UHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
+
+	SetHealth(startingHealth);
 
 	if (AActor* owner = GetOwner())
 	{
